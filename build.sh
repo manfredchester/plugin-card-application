fbade82becdd7172e4ac47267ff127e1d0cad505
zhenghao.xu <zhenghao.xu@connext.com.cn>
2020-11-24 17:31:37 +0800

    add

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
-go build -o plugin/plugin.so -buildmode=plugin plugin.go
-go build -o plugin/plugin_name.so -buildmode=plugin plugin_name.go
-go build -o plugin/greet.so -buildmode=plugin greet.go
-go build
+/data/go/bin/go build -o plugin/plugin.so -buildmode=plugin plugin/plugin.go
+/data/go/bin/go build -o plugin/plugin_name.so -buildmode=plugin plugin/plugin_name.go
+/data/go/bin/go build -o plugin/greet.so -buildmode=plugin plugin/greet.go
+/data/go/bin/go build
