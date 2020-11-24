0ad2501ca67205ff96e7962877b6b34a18007e57
Xu <Zhenghao.Xu@connext.com.cn>
2020-11-24 17:17:18 +0800

    add

--- build.sh
+++ build.sh
@@ -1,2 +1,4 @@
 go build -o plugin/plugin.so -buildmode=plugin plugin.go
+go build -o plugin/plugin_name.so -buildmode=plugin plugin_name.go
+go build -o plugin/greet.so -buildmode=plugin greet.go
 go build
--- main.go
+++ main.go
@@ -6,17 +6,14 @@ import (
 	"plugin"
 )
 
-<<<<<<< HEAD
-type Greeters interface {
-=======
 type Greeter interface {
->>>>>>> 0476bd76ddca860cd592ed3dd73f204e3295a147
 	Greet()
 }
 
 func main() {
+	fmt.Println("echo ./plugin/greet.so")
 	// 3. 查找并实例化插件
-	plug, err := plugin.Open("./greet.so")
+	plug, err := plugin.Open("./plugin/greet.so")
 	if err != nil {
 		fmt.Println(err)
 		os.Exit(1)
@@ -30,8 +27,8 @@ func main() {
 	}
 
 	// 5. 类型转换
-	var greeter Greeters
-	greeter, ok := symGreeter.(Greeters)
+	var greeter Greeter
+	greeter, ok := symGreeter.(Greeter)
 	if !ok {
 		fmt.Println(err)
 		os.Exit(1)
@@ -40,20 +37,16 @@ func main() {
 	// 6. 调用方法
 	greeter.Greet()
 
-	p, err := plugin.Open("./plugin_name.so")
+	fmt.Println("echo ./plugin/plugin_name.so")
+	p, err := plugin.Open("./plugin/plugin_name.so")
 	if err != nil {
 		panic(err)
 	}
 	fmt.Println(p)
-<<<<<<< HEAD
 	v, err := p.Lookup("V")
-=======
-	m, err := p.Lookup("Greeter")
->>>>>>> 0476bd76ddca860cd592ed3dd73f204e3295a147
 	if err != nil {
 		panic(err)
 	}
-<<<<<<< HEAD
 	f, err := p.Lookup("F")
 	if err != nil {
 		panic(err)
@@ -61,23 +54,16 @@ func main() {
 	*v.(*int) = 7
 	f.(func())() // prints "Hello, number 7"
 
-	// fmt.Println("plug start")
-	// p, err := plugin.Open("plugin/plugin.so")
-	// if err != nil {
-	// 	fmt.Println("err:", err)
-	// }
-	// fmt.Println(p)
-	// m, err := p.Lookup("Demo")
-	// if err != nil {
-	// 	fmt.Println("err:", err)
-	// }
-=======
-	greeter, ok := m.(Greeter)
-	if !ok {
+	fmt.Println("./plugin/plugin.so")
+	plugs, err := plugin.Open("./plugin/plugin.so")
+	if err != nil {
 		fmt.Println("err:", err)
 	}
-	greeter.Greet()
->>>>>>> 0476bd76ddca860cd592ed3dd73f204e3295a147
-	// res := m.(func(int) int)(30)
-	// fmt.Println(res)
+	fmt.Println(plugs)
+	m, err := p.Lookup("Demo")
+	if err != nil {
+		fmt.Println("err:", err)
+	}
+	res := m.(func(int) int)(30)
+	fmt.Println(res)
 }
